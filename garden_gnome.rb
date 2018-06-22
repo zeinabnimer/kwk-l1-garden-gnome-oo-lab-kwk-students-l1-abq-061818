11b711d3b2392b96b6afd65a503b4b9b383e630a
zeinabnimer <www.zeinab6@gmail.com>
2018-06-22 19:26:54 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,1 +1,15 @@
 # Code your instances here
+class GardenGnome
+
+  def intialize(name, age)
+    @name = name
+    @age = age
+  end
+
+def name
+  @name
+end
+
+test_gnome = GardenGnome.new
+
+   
